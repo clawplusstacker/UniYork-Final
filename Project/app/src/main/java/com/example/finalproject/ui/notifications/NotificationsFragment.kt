a5afd2d16559155dc9a30da3b44bb96bbfe487b4
Colby Beach <38380295+clawplusstacker@users.noreply.github.com>
2022-11-15 15:15:17 +0000

    Continued work on nav bar activity
    Figuring out how to go back to login activity from fragment.

--- Project/app/src/main/java/com/example/finalproject/ui/notifications/NotificationsFragment.kt
+++ Project/app/src/main/java/com/example/finalproject/ui/notifications/NotificationsFragment.kt
@@ -1,40 +1,48 @@
 package com.example.finalproject.ui.notifications
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.finalproject.MainActivity
+import com.example.finalproject.R
 import com.example.finalproject.databinding.FragmentNotificationsBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class NotificationsFragment : Fragment() {
 
-private var _binding: FragmentNotificationsBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    private var _binding: FragmentNotificationsBinding? = null
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View {
-    val notificationsViewModel =
+    private val binding get() = _binding!!
+    private lateinit var auth : FirebaseAuth
+    private val db = Firebase.firestore
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        val notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-    _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-    val root: View = binding.root
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
-    val textView: TextView = binding.textNotifications
-    notificationsViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
+        auth = FirebaseAuth.getInstance()
+
+
+        binding.signOutButton.setOnClickListener {
+            auth.signOut()
+        }
+
+        return root
     }
-    return root
-  }
 
-override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
